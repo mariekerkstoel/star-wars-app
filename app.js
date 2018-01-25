3057d3089526af68958e258216e833924044ffd7
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-25 15:31:43 +0000

    Add starwars episode get route

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res){
   res.send('First message');
 });
 
+app.get('/star_wars_episode/:episode_number?', function(req,res){
+  var episode_number = req.params.episode_number;
+  res.send('This is the page for episode ' + episode_number)
+})
+
 app.listen(3000, function(){
   console.log('App listening')
 })
